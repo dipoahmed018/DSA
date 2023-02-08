11d68f1632cb2438bb1d8a23b930f05e090d97c8
dipoahmed018 <dipoahmed018@gmail.com>
2023-02-08 22:37:47 +0600

    N-ary tree creation complete

--- linked list/linked_list.c
+++ linked list/linked_list.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-typedef struct link
+struct link
 {
     struct link *previous_item ;
     struct link *next_item;
     int item;
-} link;
+} ;
 
 
 int main () {
-    
-    link list[6];
-
-    list[0].item = 1225;
-    list[1].item = 33;
-    list[0].next_item = &list[1];
-    
-    printf("%d", list[0].item);
+    struct link object1;
+    struct link object2;
+    object1.item = 12;
+    object2.item = 25;
+    object1.next_item = &object2;
+    object2.previous_item = &object1;
+    object1.previous_item = NULL;
+    printf("%d", (1^0)%2);
 }
